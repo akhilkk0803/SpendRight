b7031a8385436a5ce4eae3d09e27301eb35af4d5
AKHIL K KULKARNI <97751536+akhilkk0803@users.noreply.github.com>
2023-11-27 13:00:52 +0530

    Delete frontend/backend directory

--- frontend/backend/routes/transaction.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const transactionController = require("../controllers/transaction");
-const isauth = require("../middleware/isauth");
-const routes = express.Router();
-routes.post("/create", isauth, transactionController.create);
-routes.get("/", isauth, transactionController.getTransactions);
-routes.get("/debt", isauth, transactionController.getDebts);
-
-module.exports = routes;
